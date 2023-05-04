47c542d1ce8378cdb598ea4525d480ff78994734
Heng-Chuan Lin <aks60808@gmail.com>
2023-05-04 16:07:54 +1000

    added font optimization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,21 @@ import "@/styles/globals.css";
 import Head from "next/head";
 import NavBar from "../../components/navbar";
 import Footer from "../../components/footer";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {" "}
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <NavBar />
-      <Component {...pageProps} />
-      <Footer />
+      <main className={inter.className}>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
     </>
   );
 }
